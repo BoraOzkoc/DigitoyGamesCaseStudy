d2d439504516f9124b036b5e1aa372f718469da7
Bora Özkoç <boraozkoc@hotmail.com>
2025-02-18 03:05:58 +0300

    Room selection Via scroll added

--- Assets/_Game/Scripts/Managers/RoomManager.cs
+++ Assets/_Game/Scripts/Managers/RoomManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
 
 public class RoomManager : MonoBehaviour
 {
@@ -8,5 +11,44 @@ public class RoomManager : MonoBehaviour
     private RoomController selectedRoom;
 
     [SerializeField]
+    private ScrollRect roomScrollRect;
+
+    [SerializeField]
     private List<RoomController> roomList = new List<RoomController>();
+
+    private bool dragStarted = false;
+
+    public void Update()
+    {
+        if (dragStarted == false && roomScrollRect.velocity.x > 0)
+        {
+            dragStarted = true;
+        }
+        if (dragStarted && roomScrollRect.velocity.x <= 0)
+        {
+            CheckClosestRoom();
+
+            dragStarted = false;
+        }
+    }
+
+    private void CheckClosestRoom()
+    {
+        Debug.Log("Checking closest room");
+        float minDistance = Mathf.Infinity;
+        RoomController closestRoom = null;
+        foreach (var room in roomList)
+        {
+            float distance = Vector3.Distance(
+                room.transform.position,
+                roomScrollRect.transform.position
+            );
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                closestRoom = room;
+            }
+        }
+        selectedRoom = closestRoom;
+    }
 }
